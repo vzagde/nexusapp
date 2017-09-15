ea483600d96557a8e269576955693edc0f64d381
vzagde <Vishalz@Apples-MacBook-Pro.local>
2017-09-15 21:49:12 +0530

    first commit

---
+++ platforms/browser/www/js/my-app.js
@@ -0,0 +1,360 @@
+var load_ui = [];
+var threed_src = '';
+var base_url = 'http://kreaserv-tech.com/mall_app/index.php/loader';
+var myApp = new Framework7({
+    pushState: false,
+    swipeBackPage: false,
+    preloadPreviousPage: false,
+    uniqueHistory: true,
+    uniqueHistoryIgnoreGetParameters: true,
+    modalTitle: 'Nexus',
+    imagesLazyLoadPlaceholder: 'img/lazyload.jpg',
+    imagesLazyLoadThreshold: 50,
+
+});
+
+var $$ = Dom7;
+
+$$(document).on('pageInit', function(e) {
+    var page = e.detail.page;
+    $('.single-item').slick();
+    $('.menu_sub').click(function() {
+        $('.menu_i,.menu_p').removeClass('menu_active');
+        $(this).find('.menu_i,.menu_p').addClass('menu_active');
+    });
+    if (page.name === 'about') {
+        myApp.alert('Here comes About page');
+    }
+})
+
+var mainView = myApp.addView('.view-main', {
+    // dynamicNavbar: true
+});
+
+$$(document).on('deviceready', function() {
+    //  var div = document.getElementById("map_box");
+    // // Initialize the map view
+    // map = plugin.google.maps.Map.getMap(div);
+    // Wait until the map is ready status.
+    // map.addEventListener(plugin.google.maps.event.MAP_READY, onMapReady);
+});
+
+myApp.onPageInit('index', function(page) {
+    $('.box_height').animate({
+        "height": "100%"
+    }, 1000);
+    $('.logo').animate({
+        "opacity": "1",
+        "top": "0"
+    }, 1000, function() {
+        $('.logo').delay(1000).animate({
+            "top": "-87%",
+            "left": "-81%",
+            "width": "15%"
+        }, 700);
+        $('.box_height').delay(1000).animate({
+            "height": "0%"
+        }, 700);
+        $('.red1').delay(1200).animate({
+            "opacity": "1",
+            "bottom": "0"
+        }, 800);
+        $('.enter_text').delay(1500).animate({
+            "opacity": "1",
+            "top": "28%"
+        }, 1200);
+        $('.login_sub').delay(2000).animate({
+            "opacity": "1",
+            "left": "38%"
+        }, 1200);
+    });
+});
+
+myApp.onPageInit('sync', function (page) {
+    $('.sync_text').animate({"opacity":"1" , "left":"21%"}, 1000);
+    $('.red2').delay(100).animate({"opacity":"1" , "right":"0"}, 700);
+    $('.sync_box').delay(500).animate({"opacity":"1" , "right":"19%"}, 800);
+    $('.skip_click').delay(400).animate({"opacity":"1" , "right":"2%"}, 500);
+})
+
+myApp.onPageInit('progress_bar', function (page) {
+    $('.bar_fill').animate({"width":"100%"}, 5000);
+})
+
+myApp.onPageInit('tabs', function (page) {
+    $('#tabs_data').empty();
+    $('#tabs_data').html(load_ui.mall_html);
+})
+
+myApp.onPageInit('mall_facts', function (page) {
+    $('#mall_logog_images').empty();
+    $("#mall_logog_images").append(load_ui.mall_logo);
+
+    $('#mall_data').empty();
+    $('#mall_data').append(load_ui.mall_fact_data).append(load_ui.anchors_html).append(load_ui.floor_plan_html).append(load_ui.walkthrough_html).append(load_ui.pictures_html).append(load_ui.transformation_html).append(load_ui.store_names_html).append(load_ui.store_html).append(load_ui.event_inner_html).append(load_ui.event_html).append(load_ui.location_html);
+
+    // Mall Facts Section Start
+    $(".hide_all_tabs_page").hide();
+    $("#mall_facts_page_dynamic").fadeIn();
+    $(".mall_facts_hide").hide();
+    $(".mall_facts_"+mall_id).fadeIn();
+    // Mall Facts Section End
+
+    $(".mall_id_floor_hide").hide();
+    $(".slider_box_floor_hide").hide();
+    $(".mall_id_floor_"+mall_id).show();
+    $(".slider_box_floor_1").show();
+
+    $('.single-item').slick();
+
+    $(".click_menu_sub").click(function(){
+        var data_disp_id = "#"+$(this).data('iddisp');
+        $(".hide_all_tabs_page").hide();
+        $(data_disp_id).fadeIn();
+        // Floor Plan Section
+        if (data_disp_id == '#floor_plan_page_dynamic') {
+            $('.single-item').slick('unslick');
+            $('.single-item').slick();
+            $(".event_select_sub").click(function(){
+                $(".event_select_sub").removeClass('event_select_active');
+                $(this).addClass('event_select_active');
+                var floor_id = $(this).data('floorid');
+                $(".mall_id_floor_hide").hide();
+                $(".slider_box_floor_hide").hide();
+                $(".slick-track, .slick-slide").css('width', '100%');
+                $(".mall_id_floor_"+mall_id).show();
+                $(".slider_box_floor_"+floor_id).show();
+                $('.single-item').slick('unslick');
+                $('.single-item').slick();
+            })
+        }
+
+        // Anchors Section
+        if (data_disp_id == '#anchors_page_dynamic') {
+            $(".mall_id_hide").hide();
+            $(".mall_id_"+mall_id).show();
+            $(".anchores_filter_data").click(function(){
+                var selected_floor_id = $("#anchors_floor_drop").val();
+                var selected_name_id = $("#anchors_name_drop").val();
+                $(".anchors_inner_sub").hide();
+                var categories = $(".anchors_inner_sub");
+
+                $(categories).each(function(){
+                    var floor_filter = $(this).data('floorid');
+                    var name_filter = $(this).data('storeid');
+                    var mall_id_filter = $(this).data('mallid');
+
+                    if (name_filter) {
+                        var name_split = name_filter.toString().split();
+                        if (name_split.indexOf(selected_name_id) !== -1 && mall_id_filter == mall_id) {
+                            $(this).show();
+                        }
+                    }
+
+                    if (floor_filter) {
+                        var floor_split = floor_filter.toString().split(",");
+                        if (floor_split.indexOf(selected_floor_id) !== -1 && mall_id_filter == mall_id) {
+                            $(this).show();
+                        }
+                    }
+                })
+            })
+        }
+
+        // Stores Section
+        if (data_disp_id == '#stores_page_dynamic') {
+            $(".mall_id_hide").hide();
+            $(".mall_id_"+mall_id).show();
+            $(".stores_filter_data").click(function(){
+                var selected_category_id = $("#stores_category_drop").val();
+                var selected_floor_id = $("#stores_floor_drop").val();
+                var selected_name_id = $("#stores_name_drop").val();
+                $(".store_sub").hide();
+                var categories = $(".store_sub");
+
+                $(categories).each(function(){
+                    var category_filter = $(this).data('categoryid');
+                    var floor_filter = $(this).data('floorid');
+                    var name_filter = $(this).data('storeid');
+                    var mall_id_filter = $(this).data('mallid');
+
+                    if (category_filter == selected_category_id && mall_id_filter == mall_id) {
+                        $(this).show();
+                    }
+
+                    if (name_filter) {
+                        var name_split = name_filter.toString().split();
+                        if (name_split.indexOf(selected_name_id) !== -1 && mall_id_filter == mall_id) {
+                            $(this).show();
+                        }
+                    }
+
+                    if (floor_filter) {
+                        var floor_split = floor_filter.toString().split(",");
+                        if (floor_split.indexOf(selected_floor_id) !== -1 && mall_id_filter == mall_id) {
+                            $(this).show();
+                        }
+                    }
+                })
+            })
+
+            $(".load_stores_inner").click(function(){
+                var data_disp_id = "#stores_inner_page_dynamic";
+                $(".hide_all_tabs_page").hide();
+                $(data_disp_id).fadeIn();
+
+                // Stores Inner Section
+                if (data_disp_id == '#stores_inner_page_dynamic') {
+                    store_category_id = $(this).data('categoryid');
+                    $(".store_mall_id_hide").hide();
+                    $(".store_mall_id_"+mall_id+store_category_id).show();
+
+                    $(".stores_categories_filter_data").click(function(){
+                        var selected_category_id = $("#stores_category_drop").val();
+                        var selected_floor_id = $("#stores_floor_drop").val();
+                        var selected_name_id = $("#stores_name_drop").val();
+                        $(".store_inner_sub").hide();
+                        var categories = $(".store_inner_sub");
+
+                        $(categories).each(function(){
+                            var category_filter = $(this).data('categoryid');
+                            var floor_filter = $(this).data('floorid');
+                            var name_filter = $(this).data('storeid');
+                            var mall_id_filter = $(this).data('mallid');
+
+                            if (category_filter == selected_category_id && mall_id_filter == mall_id) {
+                                $(this).show();
+                            }
+
+                            if (name_filter) {
+                                var name_split = name_filter.toString().split();
+                                if (name_split.indexOf(selected_name_id) !== -1 && mall_id_filter == mall_id) {
+                                    $(this).show();
+                                }
+                            }
+
+                            if (floor_filter) {
+                                var floor_split = floor_filter.toString().split(",");
+                                if (floor_split.indexOf(selected_floor_id) !== -1 && mall_id_filter == mall_id) {
+                                    $(this).show();
+                                }
+                            }
+                        })
+                    })
+                }
+            })
+        }
+
+        // Pictures Section
+        if (data_disp_id == '#pictures_page_dynamic') {
+            $(".pictures_malls_hide").hide();
+            $(".pictures_malls_"+mall_id).show();
+            $('.slider-for').slick({
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                arrows: false,
+                fade: true,
+                asNavFor: '.slider-nav'
+            });
+            
+            $('.slider-nav').slick({
+                slidesToShow: 5,
+                slidesToScroll: 1,
+                asNavFor: '.slider-for',
+                dots: false,
+                centerMode: true,
+                focusOnSelect: true
+            });
+        }
+
+        // Events Section
+        if (data_disp_id == '#event_page_dynamic') {
+            $(".display_mallid_hide").hide();
+            $(".display_mallid"+mall_id).show();
+            $(".event_sub").hide();
+            var events = $(".event_sub");
+            $(events).each(function(){
+                var startdate = new Date($(this).data('startdate'));
+                var enddate = new Date($(this).data('enddate'));
+                var cur_date = new Date();
+                if(startdate <= cur_date && enddate >= cur_date){
+                    $(this).show();
+                }
+            })
+
+            $('.event_select_sub').click(function() {
+                $('.event_select_sub').removeClass('event_select_active');
+                $(this).addClass('event_select_active');
+                var filter_id = $(this).data('filterid');
+                if (filter_id == 'current_events') {
+                    var filter = $(this).data('filter');
+                    $(".event_sub").hide();
+                    var events = $(".event_sub");
+                    $(events).each(function(){
+                        var startdate = new Date($(this).data('startdate'));
+                        var enddate = new Date($(this).data('enddate'));
+                        var cur_date = new Date();
+                        if(startdate <= cur_date && enddate >= cur_date){
+                            $(this).show();
+                        }
+                    })
+                } else if (filter_id == 'upcoming_events') {
+                    var filter = $(this).data('filter');
+                    $(".event_sub").hide();
+                    var events = $(".event_sub");
+                    $(events).each(function(){
+                        var startdate = new Date($(this).data('startdate'));
+                        var enddate = new Date($(this).data('enddate'));
+                        var cur_date = new Date();
+                        if(startdate >= cur_date && enddate >= cur_date){
+                            $(this).show();
+                        }
+                    })
+                } else if (filter_id == 'past_events') {
+                    $(".event_sub").hide();
+                    var events = $(".event_sub");
+                    $(events).each(function(){
+                        var startdate = new Date($(this).data('startdate'));
+                        var enddate = new Date($(this).data('enddate'));
+                        var cur_date = new Date();
+                        if(startdate <= cur_date && enddate <= cur_date){
+                            $(this).show();
+                        }
+                    })
+                }
+                $('.down_arrow1').fadeOut();
+                $(this).find('.down_arrow1').fadeIn();
+            });
+
+            $(".load_event_details_page").click(function(){
+                var data_disp_id = "#event_inner_page_dynamic";
+                $(".hide_all_tabs_page").hide();
+                $(data_disp_id).fadeIn();
+                // Events Inner Section
+                if (data_disp_id == '#event_inner_page_dynamic') {
+                    // eventid
+                    event_id = $(this).data('eventid');
+                    $(".event_sec_hide").hide();
+                    $(".event_sec_"+event_id).show();
+                    var cur_date = new Date();
+
+                    if(start_date_test <= cur_date && end_date_test >= cur_date){
+                        $(".dynamic_event_time").html("Current Event");
+                    }
+                    
+                    if(start_date_test >= cur_date && end_date_test >= cur_date){
+                        $(".dynamic_event_time").html("Upcoming Event");
+                    }
+                    
+                    if(start_date_test <= cur_date && end_date_test <= cur_date){
+                        $(".dynamic_event_time").html("Past Event");
+                    }
+                }
+
+                $(".go_back_to_events").click(function(){
+                    $(".event_page_dynamic").click();
+                })
+            })
+        }
+    })
+})
---
+++ www/js/util.js
@@ -0,0 +1,167 @@
+var base_url = 'http://kreaserv-tech.com/mall_app/index.php/loader';
+var mall_id = 0;
+var event_id = 0;
+var store_category_id = 0;
+var start_date_test = '';
+var end_date_test = '';
+function j2s(json) {
+    return JSON.stringify(json);
+}
+
+function goto_page(page) {
+    mainView.router.load({
+        url: page,
+        ignoreCache: false,
+    });
+}
+
+// function load_event_data() {
+// 	$('#events_html').empty();
+// }
+
+function login(){
+    var emp_code = $('#emp_code').val();
+
+    if (emp_code == '') {
+        myApp.alert('Employee code should not be blank.');
+        return false;
+    }
+
+    myApp.showIndicator();
+    $.ajax({
+        url: base_url + '/login_user',
+        type: 'POST',
+        crossDomain: true,
+        data: {
+            "emp_code": emp_code,
+        },
+    })
+    .done(function(res) {
+        myApp.hideIndicator();
+        if (res.status == 'SUCCESS') {
+            mainView.router.load({
+                url: 'sync.html',
+                ignoreCache: false,
+             });
+        } else {
+            myApp.alert('Employee Code is incorrect');
+        }
+    })
+    .fail(function(err) {
+        myApp.hideIndicator();
+        myApp.alert('Some error occurred on connecting.');
+    })
+    .always(function() {});
+}
+
+function download_image(){
+    // myApp.showIndicator();
+    $('.login_box').hide();
+    $('.progress_box').show();
+    $.ajax({
+        url: base_url + '/laod_image',
+        type: 'POST',
+        crossDomain: true,
+    })
+    .done(function(res) {
+        // $('.bar_fill').animate({"width":"100%"});
+        // myApp.hideIndicator();
+        // myApp.alert('Activating Download Process');
+        var counter_i = 1;
+        var arr_length = Number(res.download_images.length) - 1;
+        var animate_count = 100/Number(res.download_images.length);
+        var animate_counter = animate_count;
+        $.each(res.download_images, function( index, value ) {
+            // console.log(cordova.file.externalApplicationStorageDirectory);
+            $('.bar_fill').animate({"width":+animate_count+"%"});
+            var fileTransfer = new FileTransfer();
+            var uri = encodeURI("http://kreaserv-tech.com/mall_app/assets/app_assets/images/"+value.icon);
+
+            fileTransfer.download(
+                uri,
+                // cordova.file.externalApplicationStorageDirectory + 'files/download/'+value.icon,
+                cordova.file.dataDirectory + 'files/download/'+value.icon,
+                function(entry) {
+                    // myApp.alert('File ('+counter_i+') Download Completed');
+                    var fileTransfer = new FileTransfer();
+                    if (arr_length == counter_i) {
+                        // $('.progress_text').text('THANK YOU FOR DOWNLOADING ');
+                        // $('.p_t1').fadeIn();
+                    }
+            });
+             // myApp.alert(cordova.file.dataDirectory + 'files/download/'+value.icon);
+              console.log(cordova.file.dataDirectory + 'files/download/'+value.icon);
+            counter_i = counter_i + 1;
+            animate_count = animate_count + animate_counter;
+            // console.log("Index value: "+index);
+            // console.log("Array Lenght: "+arr_length);
+        })
+        var send_url = cordova.file.dataDirectory + 'files/download/';
+        myApp.showIndicator();
+        $.ajax({
+            url: base_url+"/load_ui",
+            type: 'POST',
+            crossDomain: true,
+            data: {
+                send_url : send_url,
+            }
+        })
+        .done(function(res) {
+            load_ui = res;
+            // load_location_ui();
+        })
+        .fail(function(err) {
+            myApp.hideIndicator();
+            myApp.alert('Some error occurred on connecting.');
+        })
+        .always(function() {
+            myApp.hideIndicator();
+        });
+        $('.progress_text').text('THANK YOU FOR DOWNLOADING ');
+        $('.p_t1').fadeIn();
+        // myApp.alert('Download Process Completed');
+    })
+    .fail(function(err) {
+        myApp.hideIndicator();
+        myApp.alert('Some error occurred on connecting.');
+    })
+    .always(function() {});
+}
+
+function load_mall_page(id) {
+    mall_id = Number(id);
+    goto_page('mall_facts.html');
+}
+
+function load_event_details_page(id) {
+    event_id = id;
+	start_date_test = new Date($(".display_event_id"+event_id).data('startdate'));
+	end_date_test = new Date($(".display_event_id"+event_id).data('enddate'));
+    goto_page('event_inner.html');
+}
+
+function load_stores_inner(id) {
+    store_category_id = id;
+    goto_page('store_inner.html');
+}
+
+function logout(){
+
+    mainView.router.load({
+        url: 'index.html',
+        ignoreCache: false,
+    });
+
+}
+
+function load_location_ui() {
+    mainView.router.load({
+        url: 'location.html',
+        query: {
+            loaction_load_status: 'true',
+        },
+        ignoreCache: false,
+    });
+    // $("#location_container").empty();
+    // $("#location_container").html(load_ui.location_html);
+}
